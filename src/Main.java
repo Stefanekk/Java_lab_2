6073f3ee070624dcb3a86b5f6950f8e336441f2e
Bartek Stefański <92265595+Stefanekk@users.noreply.github.com>
2022-01-21 21:19:59 +0100

    Commit cw 3

--- src/Main.java
+++ src/Main.java
@@ -14,12 +14,56 @@ public class Main {
           b) program który określi czy liczba jest podzielna przez 7
          */
 
+        Scanner scan = new Scanner(System.in);
+        int liczba = scan.nextInt();
+        switch (liczba%2){
+            case 0:
+                System.out.println("Liczba jest parzysta");
+                break;
+
+            case 1:
+                System.out.println("Liczba jest niepatrzysta");
+                break;
+        }
+        switch (liczba%7){
+            case 0 :
+                System.out.println("Liczba jest podzielna przez 7");
+                break;
+            default:
+                System.out.println("Liczba nie jest podzielna przez 7");
+                break;
+        }
         /* zad.2
            a) napisać program podobny do programu z przykładu, który ma zliczyć iloś liter 'o', 'p', 'x', 'z', 'q' w wyrazie:
             "ooooozjoonoxoooozoooooooooozoxooooopnpppoooiiooooomnzooooxoqoojoozoootyoooozoooxoooozooooxooooooojoooopxoooonnmooo"
            b) oraz ma określić ile jest pozostałych liter, które nie należą do: 'o', 'p', 'x', 'z', 'q'
          */
-
-
+        String text = "ooooozjoonoxoooozoooooooooozoxooooopnpppoooiiooooomnzooooxoqoojoozoootyoooozoooxoooozooooxooooooojoooopxoooonnmooo";
+        int[] tablica = {0,0,0,0,0,0};
+        for (char character : text.toCharArray()){
+            switch (character){
+                case 'o':
+                    ++tablica[0];
+                    break;
+                case 'p':
+                    ++tablica[1];
+                    break;
+                case 'x':
+                    ++tablica[2];
+                    break;
+                case 'z':
+                    ++tablica[3];
+                    break;
+                case 'q':
+                    ++tablica[4];
+                    break;
+                default:
+                    ++tablica[5];
+                    break;
+            }
+        }
+        System.out.printf(" 'o' wystepuje w tekscie: %d razy\n 'p' wystepuje w tekscie: %d razy\n 'x' wystepuje w tekscie: %d razy\n" +
+                " 'z' wystepuje w tekscie: %d razy\n 'q' wystepuje w tekscie: %d razy\n inne litery wystepuja w tekscie: %d razy", tablica[0],
+                tablica[1],tablica[2],tablica[3],tablica[4],tablica[5]);
     }
 }
