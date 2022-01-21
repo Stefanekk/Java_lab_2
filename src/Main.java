d25b2962eff6796c06eac38ec7ff527e1e983b47
Bartek Stefański <92265595+Stefanekk@users.noreply.github.com>
2022-01-21 21:29:57 +0100

    Commit cw 4

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
-    public static void main(String[] args) {
+    public static int main(String[] args) {
 
         /* cw.4  */
 
@@ -9,7 +9,24 @@ public class Main {
         b) w drugiej pętli continue
         c) w trzeciej pętli return
         */
-
-
+        for (int i = 1; i<5; i++){
+            if (i == 2){
+                break;
+            }
+        }
+        for (int x = 0; x <= 5; x++){
+            if (x == 2 || x == 3){
+                System.out.printf("%d\n", x);
+            }
+            else{
+                continue;
+            }
+        }
+        for (int k = 5; k >= 0; k--){
+            if (k == 2){
+                return k;
+            }
+        }
+        return 0;
     }
 }
